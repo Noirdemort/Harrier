e6426d48bea9bc3ae90b3f9c54c1f7d566c44aec
noirdemort <darkStillwater@protonmail.com>
2020-09-11 19:24:30 +0530

    add login for the application

--- Harrier/LoginViewController.swift
+++ Harrier/LoginViewController.swift
@@ -12,9 +12,54 @@ class LoginViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		self.username.text = ""
+		self.password.text = ""
+		self.cryptoSalt.text = ""
+		self.initVector.text = ""
+	}
+	
+	@IBOutlet weak var username: UITextField!
 
-
+	@IBOutlet weak var password: UITextField!
+	
+	@IBOutlet weak var cryptoSalt: UITextField!
+	
+	@IBOutlet weak var initVector: UITextField!
+	
+	
+	@IBAction func authorize(_ sender: Any) {
+		
+		guard let cryptoKey = self.password.text else { return }
+		
+		if let _ =  authenticateUser(password: cryptoKey){
+			self.performSegue(withIdentifier: "authSegue", sender: nil)
+		}
+		
+	}
+	
+	
+	@IBAction func setUpAccount(_ sender: Any){
+		self.performSegue(withIdentifier: "resetSegue", sender: nil)
+	}
+	
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		
+		if segue.identifier ?? "" == "authSegue" {
+			let destination = segue.destination as! IntelViewController
+			var translator = Translator(cryptoKey: self.password.text!,
+									plainSalt: self.cryptoSalt.text!,
+									initVector: self.initVector.text!)
+			translator.loadAndDraft(filePath: translator.filePath)
+		
+			destination.translator = translator
+		}
+		
+	}
+	
+	
 }
 
