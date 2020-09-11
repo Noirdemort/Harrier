0863141a4982ef10cf7a06d686a60d11603be272
noirdemort <darkStillwater@protonmail.com>
2020-09-11 19:27:11 +0530

    add functionality to reset keys

--- Harrier/ResetViewController.swift
+++ Harrier/ResetViewController.swift
@@ -9,22 +9,73 @@
 import UIKit
 
 class ResetViewController: UIViewController {
-
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+	
+	@IBOutlet weak var oldPassword: UITextField!
+	@IBOutlet weak var oldSalt: UITextField!
+	@IBOutlet weak var oldIV: UITextField!
+	@IBOutlet weak var newPassword: UITextField!
+	@IBOutlet weak var cryptoSalt: UITextField!
+	@IBOutlet weak var initializationVector: UITextField!
+	
+	
+	@IBAction func setUpNewKeys(_ sender: Any){
+		
+		if newPassword.text?.isEmpty ?? true {
+			return
+		}
+		
+		if cryptoSalt.text?.isEmpty ?? true || cryptoSalt.text?.count != 8 {
+			return
+		}
+		
+		
+		if initializationVector.text?.isEmpty ?? true || initializationVector.text?.count != 8  {
+			return
+		}
+		
+		
+		do {
+			
+			
+			let translating = Translator(cryptoKey: newPassword.text!,
+										 plainSalt: cryptoSalt.text!,
+										 initVector: initializationVector.text!)
+			
+			try newPassword.text!.sha3(.sha512).write(toFile: Translator.authPath,
+													  atomically: true,
+													  encoding: .utf8)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+			
+			if let password = oldPassword.text, let salt = oldSalt.text, let iv = oldIV.text {
+				let translation = Translator(cryptoKey: password,
+											 plainSalt: salt,
+											 initVector: iv)
+		
+														  
+				translating.saveAndDeploy(hunts: translation.HuntingParty, filePath: translating.filePath)
+				
+			} else {
+				
+				translating.saveAndDeploy(hunts: [], filePath: translating.filePath)
+				
+			}
+			
+			self.navigationController?.popViewController(animated: true)
+			
+		} catch {
+			print(error.localizedDescription)
+		}
+		
+	}
+	
+	
+	@IBAction func goBack(_ sender: Any){
+		self.navigationController?.popViewController(animated: true)
+	}
+	
 }
