1ae90001b5880cbea024b506b48b75876babe6c6
noirdemort <darkStillwater@protonmail.com>
2020-09-11 19:26:11 +0530

    show edit add hunt instance

--- Harrier/HunterViewController.swift
+++ Harrier/HunterViewController.swift
@@ -8,23 +8,71 @@
 
 import UIKit
 
+protocol CUDable: class {
+	func addHunt(hunt: Hunt)
+	func editHunt(hunt: Hunt, name: String, quickNotes: String, specialNotes: String)
+	func removeHunt(hunt: Hunt)
+}
+
+
 class HunterViewController: UIViewController {
 
+	var hunt: Hunt? = nil
+	weak var cudableDelegate: CUDable? = nil
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		
+		if let h = hunt {
+			self.codename.text = h.codename
+			self.codename.isEnabled = false
+			self.name.isEnabled = false
+			self.quickNotes.isEditable = false
+			self.specialNotes.isEditable = false
+		}
+		self.name.text = hunt?.name
+		self.quickNotes.text = hunt?.quickNotes
+		self.specialNotes.text = hunt?.specialNotes
+		
     }
+	
+	
+	@IBOutlet weak var isEditable: UISwitch!
     
+	@IBOutlet weak var codename: UITextField!
+	@IBOutlet weak var name: UITextField!
+	@IBOutlet weak var quickNotes: UITextView!
+	@IBOutlet weak var specialNotes: UITextView!
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	@IBAction func changeEditingStatus(_ sender: UISwitch) {
+		self.name.isEnabled = sender.isOn
+		self.quickNotes.isEditable = sender.isOn
+		self.specialNotes.isEditable = sender.isOn
+	}
+	
+	
+	@IBAction func save(_ sender: Any){
+		
+		if let hunting = hunt {
+			
+			self.cudableDelegate?.editHunt(hunt: hunting, name: self.name.text ?? "", quickNotes: self.quickNotes.text, specialNotes: self.specialNotes.text)
+			
+		} else {
+			
+			if self.codename.text?.isEmpty ?? true || self.name.text?.isEmpty ?? true {
+				return
+			}
+			
+			let newHunt = Hunt(codename: self.codename.text!, name: self.name.text!, quickNotes: self.quickNotes.text, specialNotes: self.specialNotes.text)
+			self.cudableDelegate?.addHunt(hunt: newHunt)
+		}
+		
+		self.navigationController?.popViewController(animated: true)
+	}
+	
+	
+	@IBAction func back(_ sender: Any){
+		self.navigationController?.popViewController(animated: true)
+	}
 
 }
